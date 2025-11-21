91db5dc746011e12e2a06feac6f5ad7ac83d4449
John <johncw90@yahoo.com>
2025-11-20 18:03:31 -0600

    Complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let repeatedStr = "";
+  for (let i = 0; i < num; i++) {
+    repeatedStr += str;
+  }
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    return repeatedStr;
+  }
 };
 
 // Do not edit below this line
